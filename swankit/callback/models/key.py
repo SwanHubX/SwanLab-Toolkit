e6961d8c6037e86a81bc6e84909f7288d1666034
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-10-20 01:23:28 +0800

    update:model

--- swankit/callback/models/key.py
+++ swankit/callback/models/key.py
@@ -7,7 +7,7 @@ r"""
 @Description:
     与Key相关的回调函数触发时的模型
 """
-from typing import Union, Optional, Dict, List
+from typing import Union, Optional, Dict, List, Literal
 from swankit.core import ChartType, ParseErrorInfo, MediaBuffer
 from urllib.parse import quote
 import os
@@ -20,47 +20,43 @@ class ColumnInfo:
 
     def __init__(
         self,
-        key_id: str,
         key: str,
+        key_id: str,
+        key_name: str,
+        key_class: Literal["CUSTOM", "SYSTEM"],
         section: str,
+        section_sort: int,
         chart: ChartType,
-        sort: Optional[int] = None,
+        chart_reference: Literal["step", "time"],
         error: Optional[ParseErrorInfo] = None,
-        reference: Optional[str] = None,
         config: Optional[Dict] = None,
     ):
-        self.id = key_id
         """
-        当前实验下，列的唯一id，与保存路径等信息有关
+        生成的列信息对象
+        :param key: 生成的列名称
+        :param key_id: 当前实验下，列的唯一id，与保存路径等信息有关
+        :param key_name: key的别名
+        :param key_class: 列的类型，CUSTOM为自定义列，SYSTEM为系统生成列
+        :param section: 列的组
+        :param section_sort: 列在section中的参考排序，不代表实际排序
+        :param chart: 列对应的图表类型
+        :param chart_reference: 这个列对应图表的参考系，step为步数，time为时间
+        :param error: 列的类型错误信息
+        :param config: 列的额外配置信息
         """
         self.key = key
-        """
-        列的key名称
-        """
-        self.namespace = section
-        """
-        列的命名空间
-        """
+        self.key_id = key_id
+        self.key_name = key_name
+        self.key_class = key_class
+
+        self.section = section
+        self.section_sort = section_sort
+
         self.chart = chart
-        """
-        列的图表类型
-        """
+        self.chart_reference = chart_reference
+
         self.error = error
-        """
-        列的类型错误信息
-        """
-        self.reference = reference
-        """
-        列的参考对象
-        """
-        self.sort = sort
-        """
-        列在namespace中的排序
-        """
         self.config = config if config is not None else {}
-        """
-        列的额外配置信息
-        """
 
     @property
     def got(self):
@@ -80,112 +76,86 @@ class ColumnInfo:
             return None
         return self.error.expected
 
+    @property
+    def key_encode(self):
+        """
+        对key进行url编码
+        """
+        return quote(self.key, safe="")
+
 
 class MetricInfo:
     """
-    指标信息，当新的指标被log时，会生成这个对象
+    指标信息，当新的指标数据被log时，会生成这个对象
     """
 
     __SUMMARY_NAME = "_summary.json"
 
     def __init__(
         self,
-        key: str,
-        column_info: ColumnInfo,
         error: Optional[ParseErrorInfo],
+        column_info: ColumnInfo,
         metric: Union[Dict, None] = None,
-        summary: Union[Dict, None] = None,
-        step: int = None,
-        epoch: int = None,
-        logdir: str = None,
+        metric_buffers: List[MediaBuffer] = None,
+        metric_summary: Union[Dict, None] = None,
+        metric_step: int = None,
+        metric_epoch: int = None,
         metric_file_name: str = None,
-        media_dir: str = None,
-        buffers: List[MediaBuffer] = None,
+        swanlab_logdir: str = None,
+        swanlab_media_dir: str = None,
     ):
-        self.__error = error
-
-        self.key = quote(key, safe="")
         """
-        指标的key名称，被quote编码
+        生成的指标信息对象
+        :param column_info: 此指标对应的列信息
+        :param error: 创建此指标时的错误信息
+        :param metric: 此指标的数据
+        :param metric_buffers: 此指标的媒体数据，如果为None，表示没有媒体数据
+        :param metric_summary: 此指标的摘要信息
+        :param metric_step: 此指标的步数
+        :param metric_epoch: 此指标对应本地的行数
+        :param metric_file_name: 此指标的文件名
+        :param swanlab_logdir: swanlab在本次实验的log文件夹路径
+        :param swanlab_media_dir: swanlab在本次实验的media文件夹路径
         """
+        self.error = error
         self.column_info = column_info
-        """
-        指标对应的列信息
-        """
         self.metric = metric
-        """
-        指标信息，error时为None
-        """
-        self.summary = summary
-        """
-        摘要信息，error时为None
-        """
-        self.step = step
-        """
-        当前指标的步数，error时为None
-        """
-        self.epoch = epoch
-        """
-        当前指标对应本地的行数，error时为None
-        """
-        _id = self.column_info.id
-        self.metric_path = None if self.error else os.path.join(logdir, _id, metric_file_name)
-        """
-        指标文件的路径，error时为None
-        """
-        self.summary_path = None if self.error else os.path.join(logdir, _id, self.__SUMMARY_NAME)
-        """
-        摘要文件的路径，error时为None
-        """
-        self.media_dir = media_dir
-        """
-        静态文件的根文件夹，本地保存时需要自己拼接
-        """
-        self.buffers = buffers
-        """
-        需要上传的媒体数据，比特流，error时为None，如果上传为非媒体类型（或Text类型），也为None
-        """
+        self.metric_buffers = metric_buffers
+        self.metric_summary = metric_summary
+        self.metric_step = metric_step
+        self.metric_epoch = metric_epoch
+        _id = self.column_info.key_id
+        self.metric_path = None if self.is_error else os.path.join(swanlab_logdir, _id, metric_file_name)
+        self.summary_path = None if self.is_error else os.path.join(swanlab_logdir, _id, self.__SUMMARY_NAME)
+        self.swanlab_media_dir = swanlab_media_dir
+        self.metric_buffers = metric_buffers
         # 写入文件名称，对应上传时的文件名称：{key}/{文件名称}，文件夹名称为key
-        if self.buffers is not None:
-            for i, buffer in enumerate(self.buffers):
-                buffer.file_name = "{}/{}".format(self.key, metric["data"][i])
+        if self.metric_buffers is not None:
+            for i, buffer in enumerate(self.metric_buffers):
+                buffer.file_name = "{}/{}".format(self.column_info.key_encode, metric["data"][i])
 
     @property
-    def error(self) -> bool:
+    def is_error(self) -> bool:
         """
         这条指标信息是否有错误，错误分几种：
             1. 列错误，列一开始就出现问题
             2. 重复错误
             3. 指标错误
         """
-        return self.error_info is not None or self.column_error_info is not None
+        return self.error is not None or self.column_error is not None
 
     @property
-    def column_error_info(self) -> Optional[ParseErrorInfo]:
+    def column_error(self) -> Optional[ParseErrorInfo]:
         """
         列错误信息
         """
         return self.column_info.error
 
     @property
-    def error_info(self) -> Optional[ParseErrorInfo]:
-        """
-        指标错误信息
-        """
-        return self.__error
-
-    @property
-    def duplicated_error(self) -> bool:
-        """
-        是否是重复的指标
-        """
-        return self.__error and self.__error.duplicated
-
-    @property
     def data(self) -> Union[Dict, None]:
         """
         指标数据的data字段
         """
-        if self.error:
+        if self.is_error:
             return None
         return self.metric["data"]
